3068aae173a38743777ab732a0c791d1cf14cb7a
Hack Reactor Students <robb.mitch+github@gmail.com>
2014-06-09 16:42:11 -0700

    Refactored to pseudoclassical style

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -6,7 +6,9 @@ describe("blinkyDancer", function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+
+
+    blinkyDancer =new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it("should have a jQuery $node object", function(){
@@ -23,6 +25,7 @@ describe("blinkyDancer", function() {
     it("should call step at least once per second", function(){
       sinon.spy(blinkyDancer, "step");
       expect(blinkyDancer.step.callCount).to.be.equal(0);
+
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
--- src/init.js
+++ src/init.js
@@ -22,11 +22,19 @@ $(document).ready(function(){
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    // var dancer = dancerMakerFunction(
+    //   $("body").height() * Math.random(),
+    //   $("body").width() * Math.random(),
+    //   Math.random() * 1000
+    // );
+
+    var dancer = new BlinkyDancer(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
-    );
+      );
+
+
     $('body').append(dancer.$node);
   });
 });
